d444537ab887ac3369d6af0b3a626060a2311ea7
AnonRobot <james.woodbridge@gmail.com>
2018-06-03 15:33:08 +0100

    Added more loggin

--- src/wallet.h
+++ src/wallet.h
@@ -38,6 +38,7 @@
 extern CFeeRate payTxFee;
 extern CAmount maxTxFee;
 extern unsigned int nTxConfirmTarget;
+extern bool bSpendZeroConfChange;
 extern bool bdisableSystemnotifications;
 extern bool fSendFreeTransactions;
 extern bool fPayAtLeastCustomFee;
