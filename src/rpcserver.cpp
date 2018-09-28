56d0e7aeb149bec2fa9b40bd923f478d9f41e355
Danystem <dan@dystem.io>
2018-09-28 19:54:22 +0100

    add generate functionality

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -324,6 +324,7 @@ static const CRPCCommand vRPCCommands[] =
 
 #ifdef ENABLE_WALLET
         /* Coin generation */
+        {"generating", "generate", &generate, true, false, false},
         {"generating", "getgenerate", &getgenerate, true, false, false},
         {"generating", "gethashespersec", &gethashespersec, true, false, false},
         {"generating", "setgenerate", &setgenerate, true, true, false},
